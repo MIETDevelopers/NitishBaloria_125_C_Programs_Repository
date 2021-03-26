4da0fde4e8a5e098e1abe5d7f4c87eb3f6a424e0
Nitish Baloria <2020a1r125@mietjammu.in>
2021-03-26 21:37:55 +0530

    DO WHILE FILE

---
+++ doWhile.c
@@ -0,0 +1,9 @@
+#include<stdio.h>  //preprocessor directive to include header file having standard input and output functions.
+int main(){ //main function body.
+	int i=1;  // declaration and intialisation of variable of data type int.
+	do{    // using do while loop
+		printf("%d \n",i);
+		i++;
+	}while(i<=10);
+	return 0;
+} // end of main function body.
