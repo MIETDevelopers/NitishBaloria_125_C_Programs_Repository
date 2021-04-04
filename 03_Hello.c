44effc67591928ff3d55f35fd34c7b5933a2ae5e
NitishBaloria <76279474+NitishBaloria@users.noreply.github.com>
2021-04-04 12:35:11 +0530

    Update 03_Hello.c

--- 03_Hello.c
+++ 03_Hello.c
@@ -1,4 +1,4 @@
-//A basic C Program to print Hello World.				© Ishav Verma 18/March/2021
+//A basic C Program to print Hello World.				© NITISH BALORIA 15/March/2021
 //Start
 #include<stdio.h> //Including standard input ouput header files from library.
 int main(){ 
